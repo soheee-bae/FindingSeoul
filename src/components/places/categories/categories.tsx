492102912d471ef41ae8de566282d742d14c3dfd
SoHee <soheebae@SOHEEs-MacBook-Pro.local>
2025-03-11 23:44:05 +0900

    clean

--- src/components/places/categories/categories.tsx
+++ src/components/places/categories/categories.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 export default function Categories() {
+  // const categories = places?.map((place) => place.category).flat();
+  // const cat = categories?.filter(function (item, pos) {
+  //   return categories.indexOf(item) == pos;
+  // });
+
   return <div>No results found</div>;
 }
-// const categories = places?.map((place) => place.category).flat();
-// const cat = categories?.filter(function (item, pos) {
-//   return categories.indexOf(item) == pos;
-// });
