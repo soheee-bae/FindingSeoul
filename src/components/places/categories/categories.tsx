baf5683712283ad3fdd96a2cd95e80c6f60e3de2
SoHee <amybae28@gmail.com>
2025-03-14 20:37:10 +0900

    added base category field

--- src/components/places/categories/categories.tsx
+++ src/components/places/categories/categories.tsx
@@ -1,7 +1,24 @@
 "use client";
 
-export default function Categories() {
-  return <div>No results found</div>;
+import { BASE_CATEGORIES } from "@/data/categories";
+import { useRouter } from "next/navigation";
+
+export default function BaseCategories() {
+  const router = useRouter();
+
+  const handleCategoryClick = (station: string) => {
+    router.push(`/places/${station}`);
+  };
+
+  return (
+    <div>
+      {BASE_CATEGORIES.map((category) => (
+        <div key={category.id} onClick={handleCategoryClick}>
+          {category.name}
+        </div>
+      ))}
+    </div>
+  );
 }
 // const categories = places?.map((place) => place.category).flat();
 // const cat = categories?.filter(function (item, pos) {
