fb2846197ebe0a2fd333417d4411e80e3f7de69e
SoHee <amybae28@gmail.com>
2025-03-18 20:36:27 +0900

    added clsx

--- src/components/sideBar/sideBar.tsx
+++ src/components/sideBar/sideBar.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import styles from "./sideBar.module.scss";
-import { ReactNode, useState, useRef } from "react";
-import { motion } from "framer-motion";
+import { ReactNode, useState } from "react";
 
 interface SideBarProps {
   button: ReactNode;
@@ -11,39 +10,14 @@ interface SideBarProps {
   direction?: "left" | "right";
 }
 
-const sidebar = {
-  open: (height = 1000) => ({
-    clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
-    transition: {
-      type: "spring",
-      stiffness: 20,
-      restDelta: 2,
-    },
-  }),
-  closed: {
-    clipPath: "circle(30px at 40px 40px)",
-    transition: {
-      delay: 0.5,
-      type: "spring",
-      stiffness: 400,
-      damping: 40,
-    },
-  },
-};
-
 export default function SideBar(props: SideBarProps) {
   const { button, content, className, direction = "right" } = props;
   const [isOpen, setIsOpen] = useState(false);
-  const containerRef = useRef(null);
 
   return (
-    <motion.nav
-      initial={false}
-      animate={isOpen ? "open" : "closed"}
-      ref={containerRef}
-    >
-      {content}
+    <div className={styles.container}>
       <div onClick={() => setIsOpen(!isOpen)}>{button}</div>
-    </motion.nav>
+      <div className={styles.content}>{content}</div>
+    </div>
   );
 }
