8d8de80a40e8b38cf00e3941969546ff44ba13fa
sohee28 <amybae28@gmail.com>
2025-05-05 23:58:04 +0900

    edit subway lines data

--- src/data/subwayLines.tsx
+++ src/data/subwayLines.tsx
@@ -3,5 +3,38 @@ import { SubwayLine } from "@/interfaces/subwayLines";
 export const subwayLines: SubwayLine[] = [
   {
     line: "1",
+    name: "1호선",
+  },
+  {
+    line: "2",
+    name: "2호선",
+  },
+  {
+    line: "3",
+    name: "3호선",
+  },
+  {
+    line: "4",
+    name: "4호선",
+  },
+  {
+    line: "5",
+    name: "5호선",
+  },
+  {
+    line: "6",
+    name: "6호선",
+  },
+  {
+    line: "7",
+    name: "7호선",
+  },
+  {
+    line: "8",
+    name: "8호선",
+  },
+  {
+    line: "9",
+    name: "9호선",
   },
 ];
--- src/interfaces/subwayLines.tsx
+++ src/interfaces/subwayLines.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 export interface SubwayLine {
   line: string;
   image: ReactNode;
+  name: string;
   x?: number;
   y?: number;
 }
